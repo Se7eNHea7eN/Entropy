87232c262a80dff107d11b818bceee7a17b009d7
sherocyc <sherocyc@hotmail.com>
2019-08-22 01:23:57 +0800

    unused

--- Editor/Private/EntropyMainWindow.cpp
+++ Editor/Private/EntropyMainWindow.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "EntropyMainWindow.h"
 #include "RenderWidget.h"
-#include "../../GLRenderer/Public/GLRenderer.h"
-#include "../../Direct3DRenderer/Public/Direct3DRenderer.h"
 
 EntropyMainWindow::EntropyMainWindow(QWidget *parent)
 	: QMainWindow(parent)
