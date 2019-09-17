6cba279a69520b88e9fa892d3225eb86af5e6fc0
zihao.liu <zihao.liu@asiainnovations.com>
2019-09-17 17:47:24 +0800

    make dir

--- test/Shader/vertex.bat
+++ test/Shader/vertex.bat
@@ -1,11 +1,12 @@
+md dx11
 shaderc -f %1 -o dx11/%~n1.bin --type vertex --varyingdef varying.def.sc --platform windows -p vs_5_0 -O 3
-
+md glsl
 shaderc -f %1 -o glsl/%~n1.bin --type vertex --varyingdef varying.def.sc --platform osx 
-
+md essl
 shaderc -f %1 -o essl/%~n1.bin --type vertex --varyingdef varying.def.sc --platform android
-
+md medal
 shaderc -f %1 -o metal/%~n1.bin --type vertex --varyingdef varying.def.sc --platform osx -p metal
-
+md spirv
 shaderc -f %1 -o spirv/%~n1.bin --type vertex --varyingdef varying.def.sc --platform linux -p spirv
 @pause
 
