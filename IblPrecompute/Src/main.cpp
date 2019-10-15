bc6a6d16620b71557ac39c4d6c64209b7dbc7ace
zihao.liu <zihao.liu@asiainnovations.com>
2019-10-15 19:30:52 +0800

    prefilter

--- IblPrecompute/Src/main.cpp
+++ IblPrecompute/Src/main.cpp
@@ -330,7 +330,7 @@ int main(int argc, char* argv[]) {
 	convolutionShader.setMat4("projection", captureProjection);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
-	unsigned char* pixels = (unsigned char*)malloc(irraianceTextureSize * irraianceTextureSize * 3 * 6);
+	unsigned char* pixels = new unsigned char[irraianceTextureSize * irraianceTextureSize * 3 * 6];
 	glViewport(0, 0, irraianceTextureSize, irraianceTextureSize); // don't forget to configure the viewport to the capture dimensions.
 	glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
 
@@ -353,7 +353,79 @@ int main(int argc, char* argv[]) {
 	}
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	delete pixels;
+	
+	
+	Shader prefilterShader("cubemap.vs", "prefilter.fs");
+	unsigned int prefilterMap;
+	glGenTextures(1, &prefilterMap);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, prefilterMap);
+	for (unsigned int i = 0; i < 6; ++i)
+	{
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 128, 128, 0, GL_RGB, GL_FLOAT, nullptr);
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // be sure to set minifcation filter to mip_linear 
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// generate mipmaps for the cubemap so OpenGL automatically allocates the required memory.
+	glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 
+	// pbr: run a quasi monte-carlo simulation on the environment lighting to create a prefilter (cube)map.
+  // ----------------------------------------------------------------------------------------------------
+	prefilterShader.use();
+	prefilterShader.setInt("environmentMap", 0);
+	prefilterShader.setMat4("projection", captureProjection);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+
+	
+	const int prefilterSize = 128;
+	const int prefilterDataSize =
+		prefilterSize * prefilterSize * 3 * 6
+		+ prefilterSize / 2 * prefilterSize / 2 * 3 * 6
+		+ prefilterSize / 4 * prefilterSize / 4 * 3 * 6
+		+ prefilterSize / 8 * prefilterSize / 8 * 3 * 6
+		+ prefilterSize / 16 * prefilterSize / 16 * 3 * 6;
+	
+	unsigned char* prefilterPixels = new unsigned char[prefilterDataSize];
+
+	
+	unsigned int maxMipLevels = 5;
+	int offset = 0;
+	for (unsigned int mip = 0; mip < maxMipLevels; ++mip)
+	{
+		// reisze framebuffer according to mip-level size.
+		unsigned int mipSize = prefilterSize * std::pow(0.5, mip);
+		// glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
+		// glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, mipWidth, mipHeight);
+		glViewport(0, 0, mipSize, mipSize);
+
+		float roughness = (float)mip / (float)(maxMipLevels - 1);
+		prefilterShader.setFloat("roughness", roughness);
+		for (unsigned int i = 0; i < 6; ++i)
+		{
+			prefilterShader.setMat4("view", captureViews[i]);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, prefilterMap, mip);
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			renderCube();
+			glReadPixels(0, 0, mipSize, mipSize, GL_RGB, GL_UNSIGNED_BYTE, prefilterPixels + offset);
+			offset += mipSize * mipSize * 3;
+		}
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	
+	if (bx::open(&writer, "prefilter.dds", false, &err)) { 
+		auto imageContainer = bimg::imageAlloc(getAllocator(), bimg::TextureFormat::RGB8, prefilterSize, prefilterSize, 1, 1, true, true, prefilterPixels);
+		bimg::imageWriteDds(&writer, *imageContainer, imageContainer->m_data, prefilterDataSize, nullptr);
+		bx::close(&writer);
+	}
+	
 	glfwTerminate();
 	return 0;
 }
