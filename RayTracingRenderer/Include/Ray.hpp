37e7e8071dd60ad69ddd5fa9623e6326a62b06e1
zihao.liu <zihao.liu@asiainnovations.com>
2019-10-30 19:12:30 +0800

    update rt test

--- RayTracingRenderer/Include/Ray.hpp
+++ RayTracingRenderer/Include/Ray.hpp
@@ -3,11 +3,18 @@
 using namespace Eigen;
 
 namespace Entropy {
-	class Ray
-	{
+
+
+	class Ray {
 	public:
-		Ray() {}
-		Ray(const Vector3f& a, const Vector3f& b) { A = a; B = b; }
+		Ray() {
+		}
+
+		Ray(const Vector3f& a, const Vector3f& b) {
+			A = a;
+			B = b;
+		}
+
 		Vector3f origin() const { return A; }
 		Vector3f direction() const { return B; }
 		Vector3f point_at_parameter(float t) const { return A + t * B; }
@@ -16,4 +23,81 @@ namespace Entropy {
 		Vector3f B;
 	};
 
+	struct HitRecord {
+		float t;
+		Vector3f p;
+		Vector3f normal;
+	};
+
+	class Hittable {
+	public:
+		virtual bool hit(
+			const Ray& r, float t_min, float t_max, HitRecord& rec) const = 0;
+	};
+
+
+	class Sphere : public Hittable {
+	public:
+		Sphere() {
+		}
+
+		Sphere(Vector3f cen, float r) : center(cen), radius(r) {
+		};
+		virtual bool hit(const Ray& r, float tmin, float tmax, HitRecord& rec) const;
+		Vector3f center;
+		float radius;
+	};
+
+	bool Sphere::hit(const Ray& r, float t_min, float t_max, HitRecord& rec) const {
+		Vector3f oc = r.origin() - center;
+		float a = r.direction().dot(r.direction());
+		float b = oc.dot(r.direction());
+		float c = oc.dot(oc) - radius * radius;
+		float discriminant = b * b - a * c;
+		if (discriminant > 0) {
+			float temp = (-b - sqrt(discriminant)) / a;
+			if (temp < t_max && temp > t_min) {
+				rec.t = temp;
+				rec.p = r.point_at_parameter(rec.t);
+				rec.normal = (rec.p - center) / radius;
+				return true;
+			}
+			temp = (-b + sqrt(discriminant)) / a;
+			if (temp < t_max && temp > t_min) {
+				rec.t = temp;
+				rec.p = r.point_at_parameter(rec.t);
+				rec.normal = (rec.p - center) / radius;
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	class HittableList : public Hittable {
+	public:
+		HittableList() {}
+		HittableList(Hittable** l, int n) { list = l; list_size = n; }
+		virtual bool hit(
+			const Ray& r, float tmin, float tmax, HitRecord& rec) const;
+		Hittable** list;
+		int list_size;
+	};
+
+	bool HittableList::hit(const Ray& r, float t_min, float t_max,
+		HitRecord& rec) const {
+
+		HitRecord temp_rec;
+		bool hit_anything = false;
+		double closest_so_far = t_max;
+		for (int i = 0; i < list_size; i++) {
+			if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+				hit_anything = true;
+				closest_so_far = temp_rec.t;
+				rec = temp_rec;
+			}
+		}
+		return hit_anything;
+	}
+	
 }
--- RayTracingRenderer/Src/RayTracingRenderer.cpp
+++ RayTracingRenderer/Src/RayTracingRenderer.cpp
@@ -2,6 +2,7 @@
 #include "Ray.hpp"
 #include "GL/glew.h"
 #include "Eigen/Core"
+#include <limits>
 using namespace Eigen;
 using namespace Entropy;
 Entropy::RayTracingRenderer::RayTracingRenderer(HWND hwnd) : hWnd(hwnd) {
@@ -87,15 +88,16 @@ float hit_sphere(const Vector3f& center, float radius, const Ray& r) {
 	}
 }
 
-Vector3f color(const Ray& r) {
-	float t = hit_sphere(Vector3f(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-		Vector3f N = Vector3f(r.point_at_parameter(t) - Vector3f(0, 0, -1)).normalized();
-		return 0.5 * Vector3f(N.x() + 1, N.y() + 1, N.z() + 1);
+Vector3f color(const Ray& r, Hittable* world) {
+	HitRecord rec;
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
+		return 0.5 * Vector3f(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
-	Vector3f unit_direction = Vector3f(r.direction()).normalized();
-	t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * Vector3f(1.0, 1.0, 1.0) + t * Vector3f(0.5, 0.7, 1.0);
+	else {
+		Vector3f unit_direction = r.direction().normalized();
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * Vector3f(1.0, 1.0, 1.0) + t * Vector3f(0.5, 0.7, 1.0);
+	}
 }
 
 void Entropy::RayTracingRenderer::Draw() {
@@ -112,6 +114,13 @@ void Entropy::RayTracingRenderer::Draw() {
 	Vector3f horizontal(2.0, 0.0, 0.0);
 	Vector3f vertical(0.0, 2.0, 0.0);
 	Vector3f origin(0.0, 0.0, 0.0);
+
+
+	Hittable* list[2];
+	list[0] = new Sphere(Vector3f(0, 0, -1), 0.5);
+	list[1] = new Sphere(Vector3f(0, -100.5, -1), 100);
+	Hittable* world = new HittableList(list, 2);
+	
 	glBegin(GL_POINTS);
 	glPointSize(1.0);
 	for (int j = ny - 1; j >= 0; j--) {
@@ -119,7 +128,9 @@ void Entropy::RayTracingRenderer::Draw() {
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
 			Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			Vector3f col = color(r);
+
+			Vector3f p = r.point_at_parameter(2.0);
+			Vector3f col = color(r, world);
 			glColor3f(col.x(), col.y(), col.z());
 			glVertex3f( 2*u-1, 2*v -1,0);
 		}
